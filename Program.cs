f7116230fafec277209319474cb75a701edb9a50
ewqeqrt <russciyvasia@bk.ru>
2024-04-18 14:58:01 +0700

    Update jgfd

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// 123
+using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose an operation (+, -, *, /):");
+            string operation = Console.ReadLine();
+
+            double result;
+
+            switch (operation)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot divide by zero.");
+                        return;
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation.");
+                    return;
+            }
+
+            Console.WriteLine($"The result is: {result}");
+            Console.ReadLine();
+        }
+    }
+}
